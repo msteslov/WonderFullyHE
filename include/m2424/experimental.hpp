c06f2bc0b4e05c827dee8016a2cbf01151bf15f7
Matvey Teslov <msteslov@edu.hse.ru>
2026-05-31 22:59:45 +0300

    Add explicit experimental API header

---
+++ include/m2424/experimental.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "m2424/bootstrap.hpp"
+#include "m2424/bootstrap_dft.hpp"
+#include "m2424/bootstrap_plan.hpp"
+#include "m2424/bootstrap_prototype.hpp"
+#include "m2424/bootstrap_scaling.hpp"
+#include "m2424/diagonal_transform.hpp"
+#include "m2424/eval_mod.hpp"
+
+namespace m2424 {
+
+// This header intentionally exposes research and diagnostic APIs.
+// Include m2424/m2424.hpp for the stable library surface.
+
+} // namespace m2424
